ab9dd7d7518a80624981f737f48cd5919aba9f32
Molda <hbeygel@gmail.com>
2018-04-25 21:12:19 +0300

    # created boiler plate for the app

---
+++ src/Components/LoginComponent.js
@@ -0,0 +1,17 @@
+import { React, Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+class LoginComponent extends Component {
+    
+    render() {
+        return(
+            <View>
+                <Text>This is login screen</Text>
+            </View>
+        );
+    }
+
+}
+
+export default Component
