dbfd50c17581e99ace19e8277318f3899bbd9ca7
Molda <hbeygel@gmail.com>
2018-04-28 09:53:27 +0300

    created LoginComponent

--- App.js
+++ App.js
@@ -5,41 +5,16 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View } from 'react-native';
+import LoginComponent from './src/Components/LoginComponent';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome To BroApp!
-        </Text>
+      <View>
+        <LoginComponent/>      
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -1,17 +1,69 @@
-import { React, Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { 
+    TextInput ,
+    Text, 
+    View, 
+    TouchableHighlight,
+    StyleSheet 
+} from 'react-native';
 
 
 class LoginComponent extends Component {
     
+    usernameChanged(event) {
+        console.log(event);
+    }
+
+    passwordChanged(event) {
+        console.log(event);
+    }
+
+    loginButtonPressed() {
+
+    }
+
     render() {
         return(
-            <View>
-                <Text>This is login screen</Text>
+            <View style={styles.contentView}>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={this.usernameChanged.bind(this)}
+                    placeholder={"USERNAME"} />
+                    <TextInput 
+                        style={styles.textInput}
+                        onChangeText={this.passwordChanged.bind(this)} 
+                        placeholder={"PASSWORD"} />
+                <TouchableHighlight 
+                    onPress= {this.loginButtonPressed.bind(this)}
+                    style= {styles.loginButton}>
+                    <Text>Login</Text>
+                </TouchableHighlight>
             </View>
         );
     }
-
 }
 
-export default Component
+export default LoginComponent
+
+const styles = {
+    contentView: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 10   
+    },
+    textInput: {
+        height: 44,
+        width: 300,
+        borderRadius: 5,
+        borderColor: '#343434',
+        borderWidth: 1,
+    },
+    LoginButton: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        height: 30,
+        width: 100,
+        backgroundColor: 'steelblue',
+    }
+};
